17d8f3c3650a7e975bfc2a474ec0054fa49d3236
Ugnius <ugnius.malukas@gmail.com>
2021-02-25 20:35:32 +0200

    Added styles and Webpack production configuration.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-console.log('Hello Webpack Project.');
+import './styles/main.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: path.resolve(__dirname, './src/index.js'),
@@ -9,6 +12,10 @@ module.exports = {
                 exclude: /node_modules/,
                 use: ['babel-loader'],
             },
+            {
+                test: /\.(scss|css)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
         ],
     },
     resolve: {
@@ -18,6 +25,14 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),
         filename: 'bundle.js',
     },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Hello Webpack bundled JavaScript Project',
+            template: path.resolve(__dirname, './src/index.html'),
+        })
+    ],
     devServer: {
         contentBase: path.resolve(__dirname, './dist'),
     },
