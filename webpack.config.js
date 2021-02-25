524114b2443bf92ee669426f97256a91e469628c
Ugnius <ugnius.malukas@gmail.com>
2021-02-25 20:55:49 +0200

    Added eslint.

--- webpack.config.js
+++ webpack.config.js
@@ -4,36 +4,36 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, './src/index.js'),
-    module: {
-        rules: [
-            {
-                test: /\.(js)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
-            },
-            {
-                test: /\.(scss|css)$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['*', '.js'],
-    },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js',
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Hello Webpack bundled JavaScript Project',
-            template: path.resolve(__dirname, './src/index.html'),
-        })
+  entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
     ],
-    devServer: {
-        contentBase: path.resolve(__dirname, './dist'),
-    },
+  },
+  resolve: {
+    extensions: ['*', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Hello Webpack bundled JavaScript Project',
+      template: path.resolve(__dirname, './src/index.html'),
+    }),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+  },
 };
