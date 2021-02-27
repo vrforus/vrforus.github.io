c8edbd699725d88fb245f088398abf046c128051
Ugnius <ugnius.malukas@gmail.com>
2021-02-27 20:33:05 +0200

    Added styles styles

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,39 +1,73 @@
 /* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   // BrowserRouter as Router,
   HashRouter as Router,
   Route,
 } from 'react-router-dom';
+import { Container, Navbar } from 'react-bootstrap';
 
-import Navigation from '../Navigation';
+import Logo from '../../logo.svg';
 import HomePage from '../Home';
 import SubscribePage from '../Subscribe';
 
 import * as ROUTES from '../../constants/routes';
 import { withFirebase } from '../Firebase';
 
-const Header = () => (
-  <header>Logo</header>
-);
+export const useScrollHandler = () => {
+  const [scroll, setScroll] = useState(1);
+
+  useEffect(() => {
+    const onScroll = () => {
+      const scrollCheck = window.scrollY < 100;
+      if (scrollCheck !== scroll) {
+        setScroll(scrollCheck);
+      }
+    };
+
+    document.addEventListener('scroll', onScroll);
+
+    return () => {
+      document.removeEventListener('scroll', onScroll);
+    };
+  }, [scroll, setScroll]);
+
+  return scroll;
+};
+
+const Header = () => {
+  const scroll = useScrollHandler();
+
+  return (
+    <>
+      <Navbar expand="lg" fixed="top" className={(scroll) ? 'bg-transparent' : 'scroll'}>
+        <Container>
+          <Navbar.Brand href={ROUTES.HOME}>
+            <img
+              src={Logo}
+              className="d-inline-block align-top logo"
+              alt="VRfor.us"
+            />
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 
 const Footer = () => (
-  <footer><p>© 2021 VRfor.us </p></footer>
+  <footer className="footer">
+    <Container>© 2021 VRfor.us </Container>
+  </footer>
 );
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Router>
-          <Header />
-          <Route exact path={ROUTES.HOME} component={HomePage} />
-          <Route exact path={ROUTES.SUBSCRIBE} component={SubscribePage} />
-          <Footer />
-        </Router>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <Header />
+    <Route exact path={ROUTES.HOME} component={HomePage} />
+    <Route exact path={ROUTES.SUBSCRIBE} component={SubscribePage} />
+    <Footer />
+  </Router>
+);
 
 export default withFirebase(App);
