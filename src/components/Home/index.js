963a089dd9b86af339d1395b42e27d70059a9132
Ugnius <ugnius.malukas@gmail.com>
2021-02-28 18:48:32 +0200

    Added "Coming soon"

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,15 +4,21 @@ import { Col, Container, Row } from 'react-bootstrap';
 const Home = () => (
   <div className="home">
     <div className="night">
-      <div className="shooting_star"></div>
-      <div className="shooting_star"></div>
-      <div className="shooting_star"></div>
+      <div className="shooting_star" />
+      <div className="shooting_star" />
+      <div className="shooting_star" />
     </div>
-    <Container>
-      <Row className="justify-content-md-center text-center">
+    <Container className="h-100 align-self-center">
+      <Row className="justify-content-md-center  text-center">
         <Col className="product-name">
           <h1>Our Earth</h1>
           <h2>a virtual reality simulator<br />for education</h2>
+        </Col>
+      </Row>
+
+      <Row className="justify-content-md-center text-center">
+        <Col className="coming">
+          <h1>Coming soon!</h1>
         </Col>
       </Row>
     </Container>
