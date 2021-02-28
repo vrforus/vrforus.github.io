cf542a0033d226f96dc24c6578e910ee164654fe
Ugnius <ugnius.malukas@gmail.com>
2021-02-28 15:53:52 +0200

    Added styling for the product name.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,12 +4,10 @@ import { Col, Container, Row } from 'react-bootstrap';
 const Home = () => (
   <div className="home">
     <Container>
-      <Row>
-        <Col>
-          <div className="product-name ">
-            <h1>Our Earth</h1>
-            <h2>a virtual reality simulator<br />for education</h2>
-          </div>
+      <Row className="justify-content-md-center text-center">
+        <Col className="product-name">
+          <h1>Our Earth</h1>
+          <h2>a virtual reality simulator<br />for education</h2>
         </Col>
       </Row>
     </Container>
