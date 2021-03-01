e06980e0396c0357ed56d768ee7c879004de891e
Ugnius <ugnius.malukas@gmail.com>
2021-03-01 17:21:04 +0200

    Fixed shooting stars.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,28 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 
-const Home = () => (
-  <div className="home">
-    <div className="night">
-      <div className="shooting_star" />
-      <div className="shooting_star" />
-      <div className="shooting_star" />
-    </div>
-    <Container>
-      <Row className="justify-content-md-center text-center">
-        <Col className="product-name">
-          <h1>Our Earth</h1>
-          <h2>a virtual reality simulator<br />for education</h2>
-        </Col>
-      </Row>
+const Home = () => {
+  const [shootStarts, setShootStarts] = useState(true);
 
-      <Row className="justify-content-md-center text-center">
-        <Col className="coming">
-          <h1>Coming soon!</h1>
-        </Col>
-      </Row>
-    </Container>
-  </div>
-);
+  useEffect(() => {
+    const interval = setInterval(() => setShootStarts(!shootStarts), 15000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [shootStarts]);
+
+  return (
+    <div className="home">
+      {shootStarts &&
+        <div className="night">
+          <div className="shooting_star" />
+          <div className="shooting_star" />
+          <div className="shooting_star" />
+        </div>
+      }
+      <Container>
+        <Row className="justify-content-md-center text-center">
+          <Col className="product-name">
+            <h1>Our Earth</h1>
+            <h2>
+              a virtual reality simulator
+              <br />
+              for education
+            </h2>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center text-center">
+          <Col className="coming">
+            <h1>Coming soon!</h1>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
 export default Home;
