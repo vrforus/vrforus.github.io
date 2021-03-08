74e7513d00d1b28ef18b81a5dd25dd55c1ce0292
Ugnius <ugnius.malukas@gmail.com>
2021-03-08 19:48:18 +0200

    Fixed Insights.

--- src/components/Insights/index.js
+++ src/components/Insights/index.js
@@ -1,8 +1,9 @@
-import { init } from 'insights-js';
+import { init, trackPages } from 'insights-js';
 
 class Insights {
   constructor() {
     init(process.env.INISGHTS_PROJECT_ID);
+    trackPages();
   }
 }
 
